24fa4ab6eac9bde452f470cb0938e18272333d48
Stuart Leeks <stuartle@microsoft.com>
2019-01-08 16:19:01 +0000

    WIP fix

--- internal/pkg/keybindings/shell_darwin.go
+++ internal/pkg/keybindings/shell_darwin.go
@@ -6,6 +6,6 @@ import (
 
 // OpenEditor opens the specified file in the default editor
 func OpenEditor(filename string) error {
-	cmd := exec.Command("open" filename)
+	cmd := exec.Command("open",	 filename)
 	return cmd.Run()
 }
